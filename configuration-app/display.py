e1ac361606713bafe4b7283037400e33f9a4eb3f
Sam Lane <sam@Budgie-20.10>
2021-01-15 23:35:14 -0500

    Efficiency and safety tweaks
    - current mode and gpu mem are now retrieved at the same time, instead
    of running pibootctl twice
    
    - if config.txt contains "dtoverlay=vc4-kms-v3d-pi4", disable setting of
    video mode so pibootctl doesn't add a conflicting video mode
    
    - if pkexec prompt is cancelled, configuration app no longer thinks the
    display setting was successfully updated

--- configuration-app/display.py
+++ configuration-app/display.py
@@ -7,6 +7,7 @@ class Display:
     PIBOOTCTL = '/usr/bin/pibootctl'
     MODE_ARG  = 'video.firmware.mode'
     MEM_ARG   = 'gpu.mem'
+    CONFIG    = '/boot/firmware/config.txt'
 
     MODE = ['fkms', 'kms', 'legacy']
     MEM  = ['128', '256', '512']
@@ -35,53 +36,76 @@ class Display:
 
         if Overclock.is_raspi(None):
             self.load_initial()
+            if not self.safe_to_change_mode():
+                self.disable_mode_selection()
         else:
             self.displaygrid.set_visible(False)
 
     def on_modebutton_clicked(self, *args):
         for i in range(len(self.moderadiobuttons)):
             if self.moderadiobuttons[i].get_active() and self.MODE[i] != self.current_mode:
-                self.current_mode = self.MODE[i]
-                self.run_pibootctl('set', self.MODE_ARG+'='+self.MODE[i])
-                self.rebootlabel.set_visible(True)
+                if self.run_pibootctl('set', self.MODE_ARG+'='+self.MODE[i]) != 'error':
+                    self.current_mode = self.MODE[i]
+                    self.rebootlabel.set_visible(True)
 
     def on_membutton_clicked(self, *args):
         for i in range(len(self.memradiobuttons)):
             if self.memradiobuttons[i].get_active() and self.MEM[i] != self.current_mem:
-                self.current_mem = self.MEM[i]
-                self.run_pibootctl('set', self.MEM_ARG+'='+self.MEM[i])
-                self.rebootlabel.set_visible(True)
+                if self.run_pibootctl('set', self.MEM_ARG+'='+self.MEM[i]) != 'error': 
+                    self.current_mem = self.MEM[i]
+                    self.rebootlabel.set_visible(True)
 
     def load_initial(self):
-        result = self.run_pibootctl('get', self.MODE_ARG)
+        result = self.run_pibootctl('get', self.MODE_ARG, self.MEM_ARG, '--shell')
         # Hide Display tab if pibootctl is mising
-        if result == 'not found':
+        if result == 'error':
             print("Unable to run pibootctl")
             self.displaygrid.set_visible(False)
         else:
+            mode, mem = result.replace('=','\n').splitlines()[1::2]
             for i in range(len(self.moderadiobuttons)):
-                if result == self.MODE[i]:
+                if mode == self.MODE[i]:
                     self.moderadiobuttons[i].set_active(True)
                     self.current_mode = self.MODE[i]
-            result = self.run_pibootctl('get', self.MEM_ARG)
             for i in range(len(self.memradiobuttons)):
-                if result == self.MEM[i]:
+                if mem == self.MEM[i]:
                     self.memradiobuttons[i].set_active(True)
                     self.current_mem = self.MEM[i]
 
-    def run_pibootctl(self, method, param):
+    def run_pibootctl(self, method, *params):
         if method == 'set':
-            args = ['pkexec']
+            args = ['pkexec', self.PIBOOTCTL, method]
         else:
-            args = []
-        args += [self.PIBOOTCTL, method, param]
+            args = [self.PIBOOTCTL, method]
+        for item in params:
+            args.append(item)
 
         try:
             output = subprocess.check_output(args,
                 stderr=subprocess.STDOUT).decode("utf-8").strip('\'\n')
         except subprocess.CalledProcessError as e:
             output = e.output.decode("utf-8")
+            return 'error'
         except FileNotFoundError:
-            output = 'not found'
+            return 'error'
 
         return output
+
+    def disable_mode_selection(self):
+        # Disable mode buttons when pibootctl can't be safely used
+        for widget in ([self.modebutton] + self.moderadiobuttons):
+            widget.set_sensitive(False)
+            widget.set_tooltip_text("Can't change mode from vc4-kms-v3d-pi4")
+
+    def safe_to_change_mode(self):
+        # Check if video mode is currently vc4-kms-v3d-pi4
+        safe = True
+        try:
+            with open(self.CONFIG) as file:
+                content = file.readlines()
+        except IOError:
+            return True
+        for line in content:
+            if 'dtoverlay=vc4-kms-v3d-pi4' in line and line.strip()[0] != '#':
+                safe = False
+        return safe
